1f4a1e569d7d8c3c54d66a31c7a339dc1f23beaa
jito <jitokj3@gmail.com>
2020-11-28 22:37:17 +0530

    added sidebar component

--- src/components/Telegram/Sidebar/Sidebar.js
+++ src/components/Telegram/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./Sidebar.css";
+import SearchIcon from '@material-ui/icons/Search';
+import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined';
+import {IconButton} from "@material-ui/core";
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__search">
+                <SearchIcon className="sidebar__searchIcon" />
+                    <input type="text" placeholder="Search" className="sidebar__input"/>
+                </div>
+                <IconButton variant="outlined" id="sidebar__button">
+                    <BorderColorOutlinedIcon />
+                    </IconButton>
+            </div>
+            <div className="sidebar__threads">
+                Threads
+            </div>
+            <div className="sidebar__bottom"></div>
         </div>
     );
 };
