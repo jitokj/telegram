f92a0c6e43f40d1b50e21130378bb1132fc3d3aa
jito <jitokj3@gmail.com>
2020-11-28 22:44:07 +0530

    added thread content

--- src/components/Telegram/Sidebar/SidebarThread/SidebarThread.js
+++ src/components/Telegram/Sidebar/SidebarThread/SidebarThread.js
@@ -1,10 +1,14 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import "./SidebarThread.css";
 
 const SidebarThread = () => {
     return (
         <div className="sidebarThread">
-            sidebarThread
+        <Avatar />
+        <h3>Thrad Name</h3>
+        <p>Info</p>
+        <small>timestamp</small>
         </div>
     );
 };
