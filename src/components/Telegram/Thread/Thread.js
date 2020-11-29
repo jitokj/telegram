aa1c4a314b0f48eabd75bde9100f4da45eab82e2
jito <jitokj3@gmail.com>
2020-11-29 13:13:11 +0530

    added send message

--- src/components/Telegram/Thread/Thread.js
+++ src/components/Telegram/Thread/Thread.js
@@ -1,9 +1,15 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { MoreHoriz } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 import "./Thread.css";
 
 const Thread = () => {
+    const [input,setInput] = useState("");
+    const sendMessage = (e)=>{
+        e.preventDefault();
+        console.log(input);
+        setInput("");
+    }
     return (
         <div className="thread">
          <div className="thread__header">
@@ -17,6 +23,11 @@ const Thread = () => {
           <IconButton>
               <MoreHoriz className="thread__header__details" />
           </IconButton>
+         </div>
+         <div className="thread__messages"></div>
+         <div className="thread__input">
+             <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}} placeholder="write a message..." />
+             <button onClick={sendMessage}>Send</button>
          </div>
         </div>
     );
